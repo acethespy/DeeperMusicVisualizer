e8a74d1f1695c479a7f4cf5d19cf2b26b1754e54
Richard Mao <richard.mao.123@gmail.com>
2021-04-16 00:17:28 +0000

    added fix to run inference with just an audio file

--- rhythm/timing.py
+++ rhythm/timing.py
@@ -6,17 +6,62 @@
 
 import numpy as np
 from os_tools import run_command
+import subprocess
+import os
 import re
+
+from subprocess import PIPE, run
+import essentia
+from essentia.standard import *
+
+def out(command):
+    result = run(command, stdout=PIPE, stderr=PIPE, universal_newlines=True, shell=True)
+    return result.stderr
+
+
 
 def get_timing(music_path):
     """
     Obtain timing by running TimingAnlyz.exe
     """
-    result = run_command(["./TimingAnlyz.exe", music_path, "0"]).decode("utf-8")
-    bpm = float(re.findall("BPM:\W*([0-9\.]+)", result)[0])
-    ofs = float(re.findall("Offset:\W*([0-9\.]+)", result)[0])
+    #full_path = os.path.abspath("TimingAnalyz.exe")
+    #bashCommand = f"./TimingAnalyz.exe {music_path} 0"
+    #import subprocess
+    #process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE)
+    #output, error = process.communicate()
+    #"content/drive/MyDrive/cs194/DeeperMusicVisualizer/rhythm/TimingAnalyz.exe"
+    #print(output)
+    #bashcommand = f"{full_path} {music_path} 0"
+    #print(bashcommand)
+    #result = os.system(bashcommand) #.stdout.read() #decode("utf-8")
+    #test= subprocess.check_output(bashcommand, shell=True)
+    #test = os.popen(bashcommand).read()
+    #test = out(bashcommand)
+    #print(result)
+    #print("??",test)
+    
+    #result = run_command(bashcommand).decode("utf-8")#["TimingAnalyz.exe", music_path, "0"]).decode("utf-8")
+    #bpm = float(re.findall("BPM:\W*([0-9\.]+)", result)[0])
+    #ofs = float(re.findall("Offset:\W*([0-9\.]+)", result)[0])
+    
+    
+    # Loading audio file
+    audio = MonoLoader(filename=music_path)()
+
+    # Compute beat positions and BPM
+    rhythm_extractor = RhythmExtractor2013(method="multifeature")
+    bpm, beats, beats_confidence, _, beats_intervals = rhythm_extractor(audio)
+    ofs = beats[0]*1000
+    print("BPM:", bpm)
+    #print("Beat positions (sec.):", beats)
+    #print("Beat estimation confidence:", beats_confidence)
+    print("Offset:", ofs)   
+    
     if np.abs(bpm - np.round(bpm)) < 0.05:
-        result = run_command(["./TimingAnlyz.exe", music_path, str(np.round(bpm))]).decode("utf-8")
-        bpm = float(re.findall("BPM:\W*([0-9\.]+)", result)[0])
-        ofs = float(re.findall("Offset:\W*([0-9\.]+)", result)[0])
+        #result = run_command(["./TimingAnlyz.exe", music_path, str(np.round(bpm))]).decode("utf-8")
+        #bpm = float(re.findall("BPM:\W*([0-9\.]+)", result)[0])
+        #ofs = float(re.findall("Offset:\W*([0-9\.]+)", result)[0])
+        bpm = np.round(bpm)
+        
+    print("Final BPM:", bpm)
     return bpm, ofs
